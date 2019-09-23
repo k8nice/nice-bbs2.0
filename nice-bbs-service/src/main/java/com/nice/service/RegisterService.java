e83cc036e60036fd920beba586643a16d8d4e7f2
ningh <996099092@qq.com>
2019-09-23 21:21:49 +0800

    修改注释

--- nice-bbs-service/src/main/java/com/nice/service/RegisterService.java
+++ nice-bbs-service/src/main/java/com/nice/service/RegisterService.java
@@ -2,10 +2,24 @@ package com.nice.service;
 
 import com.nice.domain.BbsUser;
 
+/**
+ * 注册逻辑接口
+ * @author ningh
+ */
 public interface RegisterService {
 
+    /**
+     * 添加用户
+     * @param bbsUser
+     * @return
+     */
     String addBbsUserService(BbsUser bbsUser);
 
+    /**
+     * 检测用户是否存在
+     * @param bbsUserName
+     * @return
+     */
     Boolean checkBbsUserIsExist(String bbsUserName);
 
 }
--- nice-bbs-service/src/main/java/com/nice/service/impl/LoginServiceImpl.java
+++ nice-bbs-service/src/main/java/com/nice/service/impl/LoginServiceImpl.java
@@ -22,10 +22,14 @@ public class LoginServiceImpl implements LoginService {
     @Autowired
     private BbsUserMapper bbsUserMapper;
 
+    /**
+     * 日志
+     */
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginServiceImpl.class);
 
     /**
      * 登录用户
+     * 实现逻辑:再加密一遍，跟数据库中的bbsUserPassword进行比较
      * @param bbsUserName bbs用户名
      * @param bbsUserPassword bbs用户密码
      * @return  true or false
@@ -40,6 +44,7 @@ public class LoginServiceImpl implements LoginService {
         } catch (NullPointerException exception) {
             LOGGER.error("发生空指针，堆栈信息为{}",exception.getStackTrace());
         }
+        //如果密码正确返回true
         String bbsUserPassword1 = Md5Utils.getMd5Password(bbsUserPassword,salt);
         if (bbsUserPassword1.equals(bbsUser.getBbsUserPassword())){
             LOGGER.info("{}的密码验证通过",bbsUserName);
